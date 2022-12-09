861d291c9f86b0fd4d693792f21eb429e16aaa60
Nick Wilmes <nicholas.wilmes@disney.com>
2022-12-09 12:37:51 -0800

    day 8 of 2022 part 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from aocd import data
+import math
 
 
 ####################
@@ -23,7 +24,6 @@ test_b_answer = "8"
 class Tree:
     visible: bool = False
     height: int = 0
-    score: int = 0
 
 
 UP = 1
@@ -88,25 +88,60 @@ def part_a(content):
     
 
 def get_score(forest: list[list[Tree]], pos: tuple[int, int]) -> int:
-    return 0
+    height = forest[pos[1]][pos[0]].height
+    scores = []
+
+    # check left
+    count = 0
+    for c in reversed(range(pos[0])):
+        count += 1
+        if forest[pos[1]][c].height >= height:
+            break
+    scores.append(count)
+    
+    # check right
+    count = 0
+    for c in range(pos[0]+1, len(forest[0])):
+        count += 1
+        if forest[pos[1]][c].height >= height:
+            break
+    scores.append(count)
+
+    # check down
+    count = 0
+    for r in range(pos[1]+1, len(forest)):
+        count += 1
+        if forest[r][pos[0]].height >= height:
+            break
+    scores.append(count)
+
+    # check up
+    count = 0
+    for r in reversed(range(pos[1])):
+        count += 1
+        if forest[r][pos[0]].height >= height:
+            break
+    scores.append(count)
+    
+    return math.prod(scores)
 
 
 def part_b(content):
     high_score = 0
     forest = convert_input_to_trees(content)
-    for r in range(1, len(forest)-1):
-        for c in range(1, len(forest[1])-1):
+    check_dir(forest, LEFT)
+    check_dir(forest, RIGHT)
+    check_dir(forest, UP)
+    check_dir(forest, DOWN)
+    for r in range(0, len(forest)):
+        for c in range(0, len(forest[1])):
+            if not forest[r][c].visible:
+                continue
             score = get_score(forest, (c, r))
             if score > high_score:
                 high_score = score
 
     return high_score
-
-    ans = 0
-    for row in forest:
-        ans += len(list(filter(lambda t: t.visible, row)))
-
-    return ans
 
 
 ####################
